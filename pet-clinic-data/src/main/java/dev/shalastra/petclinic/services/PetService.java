778220f1c8ffd08d3e9fc661e608d44771f310b5
Szymon.halastra@boldare.com <Szymon.halastra@boldare.com>
2020-01-07 16:56:07 +0100

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/dev/shalastra/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package dev.shalastra.petclinic.services;
+
+import dev.shalastra.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/dev/shalastra/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package dev.shalastra.petclinic.services;
+
+import dev.shalastra.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
