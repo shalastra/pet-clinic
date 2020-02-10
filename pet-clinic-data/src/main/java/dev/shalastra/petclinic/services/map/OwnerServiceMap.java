a1be0968665baa097fea6cae0f6c50a79a1d1bee
szymon Halastra <szymon.halastra@gmail.com>
2020-02-10 17:47:39 +0100

    Impl Map based Services, closes #6

---
+++ pet-clinic-data/src/main/java/dev/shalastra/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package dev.shalastra.petclinic.services.map;
+
+import dev.shalastra.petclinic.model.Owner;
+import dev.shalastra.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/dev/shalastra/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package dev.shalastra.petclinic.services.map;
+
+import dev.shalastra.petclinic.model.Vet;
+import dev.shalastra.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
